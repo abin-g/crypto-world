b99f67d530b2798b63e90ca28584c22d9898c08f
Abin G <abings002@gmail.com>
2022-05-25 23:59:13 +0530

    Live API Added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,20 +7,28 @@ import LatestCollection from '../components/layouts/explore/LatestCollection'
 import dataCollections from '../assets/site-data/dataCollections'
 
 const Home = () => {
-    const [users, setUsers] = useState([])   
-    const [isLoading, setIsLoading] = useState(false)
-    const STRAPI_BASE_URL = process.env.REACT_APP_STRAPI_APIURL;
-    
+    //const [users, setUsers] = useState([])   
+    //const [isLoading, setIsLoading] = useState(false)
+    const STRAPI_API = process.env.REACT_APP_STRAPI_API;
+
+    const url = "https://www.develop-sr3snxi-l2plkcrdxzklc.us-3.platformsh.site/add-articles?_limit=6"
     const fetchData = () => {
-        setIsLoading(true)
-        fetch(`${STRAPI_BASE_URL}/api/add-articles?populate=*`)
-          .then(response => {
-            return response.json()
+        fetch(url, {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': 'Bearer' + STRAPI_API
+            },
           })
+          .then(response => response.json())
           .then(data => {
-            setIsLoading(false)
-            setUsers(data)
+            console.log("Response Data:",data);      
+            
           })
+          .catch(error => {
+            console.error(error)
+          }
+            );
       }
     
 
@@ -28,8 +36,8 @@ const Home = () => {
         fetchData()
       }, [])
 
-      const blodata = users.data; console.log(blodata)
-      if (!blodata) return null;
+      //const blodata = users.data; console.log(blodata)
+     // if (!blodata) return null;
 
     return <div>
         <Header />
@@ -50,25 +58,21 @@ const Home = () => {
                             </div>
                         </div>
                     </div>
-                      {isLoading && <p><center>Loading...</center></p>}
         
-                    {blodata.length > 0 && ( 
-                        <>
-                        {blodata.slice(0, 6).map(data => (
-                            <div key={data.id} className="col-lg-4 col-md-6">
+                            <div key="" className="col-lg-4 col-md-6">
                                 <article className="sc-card-article">
                                     <div className="card-media">
-                                        <Link to="/blog-details"><img src={`${STRAPI_BASE_URL}${data.attributes.Thumbnail.data.attributes.url}`} loader="lazy" style={{height:'240px'}} alt="Maximo" /></Link>
+                                        <Link to="/blog-details"><img src="" loader="lazy" style={{height:'240px'}} alt="Maximo" /></Link>
                                     </div>
                                     <div className="content">
                                         <div className="meta-info">
                                             <div className="item author">
-                                                <Link to="/authors">{data.attributes.Author}</Link>
+                                                <Link to="/authors">Author</Link>
                                             </div>
-                                            <div className="item date">{data.attributes.createdAt}</div>
+                                            <div className="item date">Date</div>
                                         </div>
                                         <div className="text-article">
-                                            <h5><Link to="/blog-details">{data.attributes.Title}</Link></h5>
+                                            <h5><Link to="/blog-details">Title</Link></h5>
                                         </div>
                                         <Link to="/blog-details"
                                             className="sc-button btn-bordered-white style letter"><span>Read More</span></Link>
@@ -76,9 +80,6 @@ const Home = () => {
                                 </article>
                             </div>
                        
-                       ))} </>
-                       
-                       )}
                 </div>
             </div>
         </div>
